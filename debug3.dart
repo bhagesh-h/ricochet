4657a44a5067e8fc69cae85984ed25c811095488
bhagesh-h <hunakuntibhagesh@gmail.com>
2026-04-12 12:24:15 +0530

    removed test files

--- debug3.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:Ricochet/models/pipeline_file.dart';
-import 'dart:io';
-
-void main() async {
-  final content = await File('C:\\Users\\bhage\\OneDrive\\Documents\\code\\ricochet\\lib\\models\\pipeline_file.dart').readAsString();
-  print('Loaded model');
-}
